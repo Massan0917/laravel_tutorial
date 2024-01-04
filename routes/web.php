e8f03eda5eec7cd849c59fa4e483d30af7c21e53
massan0917 <massan000917@gmail.com>
2024-01-04 14:34:25 +0900

    Chirpの作成

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/chirps');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
